cbe6f3f377625836233b7e7e728d7ca36ef5941d
Aqib-Javed-97 <aqibjaved1997@gmail.com>
2021-08-07 21:52:37 +0530

    adding_element_at_given_position

---
+++ arrays/adding_element_at_given_position.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,n,pos,val,temp;
+    printf("Enter the size of array ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter array elements\n");
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter the position at which value is to be added: ");
+    scanf("%d",&pos);
+    printf("Enter the value to be added: ");
+    scanf("%d",&val);
+    for(i=n-1;i>pos;i--){
+        temp=arr[i];
+        arr[i]=arr[i-1];
+        arr[i+1]=temp;
+    }
+    arr[pos]=val;
+    for(i=0;i<n+1;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
